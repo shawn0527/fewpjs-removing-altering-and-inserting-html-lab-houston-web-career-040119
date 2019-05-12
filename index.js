eef53ca243fd2b070b71b85e43348c703f45603c
Shawn Ren <shawnren527@gmail.com>
2019-05-12 04:32:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 document.querySelector('main').remove()
 var newHeader = document.createElement('h1')
 newHeader.id = 'victory'
-document.body.appnedChild(newHeader)
 newHeader.innerHTML = 'is the champion'
+document.body.appnedChild(newHeader)

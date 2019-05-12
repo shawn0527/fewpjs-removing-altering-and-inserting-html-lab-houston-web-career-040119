75b00a2ff6b958d1d95520733460d058d0ab6327
Shawn Ren <shawnren527@gmail.com>
2019-05-12 04:27:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code here!
 document.querySelector('main').remove()
 var newHeader = document.createElement('h1')
-document.body.appnedChild(newHeader)
 newHeader.id = 'victory'
+document.body.appnedChild(newHeader)
 newHeader.innerHTML = 'H1 is the champion'

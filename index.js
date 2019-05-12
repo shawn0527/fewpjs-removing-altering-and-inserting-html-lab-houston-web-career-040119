ff0f6d5b7be67aa4f85f9ecd86a8065dfd7ecfc7
Shawn Ren <shawnren527@gmail.com>
2019-05-12 04:32:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector('main').remove()
+var newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'is the champion'
+document.body.appnedChild(newHeader)
